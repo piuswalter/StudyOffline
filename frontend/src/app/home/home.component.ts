898f976c31c1c8a28f8d782b06c384c0d66fbb5b
Philipp <dev@p-fruck.de>
2021-04-18 15:14:50 +0200

    fixed image rendering

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DbService } from '../_services/db.service';
 import { Subject } from '../_models/subject.class';
 import { Flashcard } from '../_models/flashcard.class';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -11,21 +12,16 @@ import { Flashcard } from '../_models/flashcard.class';
 export class HomeComponent {
   subjectId = 0;
   hideAnswer = true;
-  question = '';
-  answer = '';
+  question: SafeHtml = '';
+  answer: SafeHtml = '';
   private cardIndex = 0;
   private subjectMap: { [key: number]: Subject } = {};
   private flashcards: Flashcard[] = [];
 
-  get subject(): Subject {
-    return this.subjectMap[this.subjectId];
-  }
-
-  get subjects(): Subject[] {
-    return Object.values(this.subjectMap);
-  }
-
-  constructor(private dbService: DbService) {
+  constructor(
+    private dbService: DbService,
+    private domSanitizer: DomSanitizer
+  ) {
     this.dbService
       .getSubjects()
       .then((subjects) => {
@@ -39,23 +35,34 @@ export class HomeComponent {
       .catch((err) => console.error(err));
   }
 
+  get subject(): Subject {
+    return this.subjectMap[this.subjectId];
+  }
+
+  get subjects(): Subject[] {
+    return Object.values(this.subjectMap);
+  }
+
   async switchSubject(subjectId: number): Promise<void> {
     if (this.subjectMap[subjectId]) {
       this.subjectId = subjectId;
+      this.flashcards = await this.dbService.getFlashcards(this.subjectId);
+      this.cardIndex = 0; // reset index to circumvent array index out of bounds
+      this.renderCard();
     }
-    this.flashcards = await this.dbService.getFlashcards(this.subjectId);
-    this.renderCard();
   }
 
   renderCard(): void {
     this.hideAnswer = true;
     if (this.flashcards[this.cardIndex]) {
       const { question, answers } = this.flashcards[this.cardIndex];
-      this.question = question;
+      this.question = this.domSanitizer.bypassSecurityTrustHtml(question);
       if (answers.length !== 1) {
         this.answer = '<p>Multiple Choice has not implemented yet</p>';
       } else {
-        this.answer = answers[0].text;
+        this.answer = this.domSanitizer.bypassSecurityTrustHtml(
+          answers[0].text
+        );
       }
     }
   }
